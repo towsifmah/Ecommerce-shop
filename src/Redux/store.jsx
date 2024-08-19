350757951d1ec28b296d20683bf6fd3a3a6df0f6
Mahmud hasan <mahmudtarak24@gmail.com>
2024-08-19 23:01:28 +0600

    f

--- src/Redux/store.jsx
+++ src/Redux/store.jsx
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartSlice from "./cartSlice";
 import productSlice from "./productSlice";
-
+import "/src/main.jsx"
 const store = configureStore({
   reducer: {
     cart: cartSlice,
