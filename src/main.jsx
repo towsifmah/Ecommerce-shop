302c4aef56e9737b9cd7491ba66b97e6d7094f41
Mahmud hasan <mahmudtarak24@gmail.com>
2024-08-19 22:52:48 +0600

    ffff

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App";
-import store from "./redux/store";
+import store from "./redux/store.jsx";
 import "./index.css";
 
 const rootElement = document.getElementById("root");
